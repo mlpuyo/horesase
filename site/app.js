913cbadbbd4300be213eefc392799e1a1eef67ee
tgck <taniguchiko@gmail.com>
2014-10-05 03:14:41 +0900

    delete UI

--- site/app.js
+++ site/app.js
@@ -70,9 +70,3 @@ function search(){
 	var url = googleImageSearchURL + googleQuery + encodeURI(keyword);
 	$(location).attr("href", url);
 }
-
-// query parameta
-// http://www13.plala.or.jp/bigdata/google.html
-
-// https://www.google.co.jp/search?tbm=ischsite:jigokuno.com %E3%81%B9%E3%81%A4%E3%81%AB
-// https://www.google.co.jp/search?tbm=isch&q=site:jigokuno.com%20%E5%88%A5%E3%81%AB
