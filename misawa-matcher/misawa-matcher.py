a05e4d125a749309f83f926c0fd273fdae30d750
garytaro <gary.taro@gmail.com>
2015-01-12 18:24:11 +0900

    mecabを利用した主な関数をmecab_funcに分離

--- misawa-matcher/misawa-matcher.py
+++ misawa-matcher/misawa-matcher.py
@@ -7,9 +7,7 @@ import sys
 import nltk
 import pickle
 import webbrowser
-import unicodedata
-import MeCab
-from subprocess import Popen, PIPE
+import mecab_func
 
 '''
 コマンドライン引数から読み込んだ文章で類似度検索
@@ -17,128 +15,6 @@ mecabのpythonバインディングのインストールは次から(mac, linux)
 pip install mecab-python3
 '''
 
-
-def breakdown_into_validwords2(sentence):
-    '''
-    mecabを使って形態素解析
-    pythonバインディングを利用しない
-    '''
-    ret_list = []
-
-    if sentence == '' or not(isinstance(sentence, str)):
-        return ret_list
-
-    sentence = sentence.replace("\n", "")
-
-    with open('.input.txt', 'w') as f:
-        # 適当な例外処理
-        try:
-            f.write(sentence)
-        except:
-            return ret_list
-
-    # 無理やりmecabで処理
-    p = Popen(["mecab", "-Ochasen", ".input.txt"], stdout=PIPE)
-    while 1:
-        line = p.stdout.readline()[:-1]
-        if os.name == 'nt':
-            line = line.decode('cp932')
-        else:
-            line = line.decode('utf-8')
-
-        if line[:3] == "EOS":
-            break
-        if not line:
-            continue
-
-        line = line.split('\t')
-        word = line[2]
-
-        # 漢字でない一文字のwordは無視
-        # 'ー'や'*'も同様
-        if len(word) == 1 and unicodedata.name(word[0])[0:4] != 'CJK ':
-            continue
-        # 二文字のひらがなは無視
-        if (len(word) == 2 and unicodedata.name(word[0])[0:4] == 'HIRA'
-                and unicodedata.name(word[1])[0:4] == 'HIRA'):
-            continue
-        if (line[3][:2] == '名詞' or line[3][:2] == '動詞'
-                or line[3][:2] == '副詞' or line[3][:3] == '形容詞'):
-            ret_list.append(word)
-
-    p.wait()
-    return ret_list
-
-
-def breakdown_into_validwords(sentence):
-    '''
-    mecabを使って形態素解析
-    pythonバインディングを利用
-    '''
-    ret_list = []
-
-    if sentence == '' or not(isinstance(sentence, str)):
-        return ret_list
-
-    sentence = sentence.replace("\n", "")
-
-    model = MeCab.Model_create("-Ochasen")
-    tagger = model.createTagger()
-    lines = tagger.parse(sentence).split('\n')
-    for line in lines:
-        if line == "EOS":
-            break
-        line = line.split('\t')
-        word = line[2]
-
-        # 漢字でない一文字のwordは無視
-        # 'ー'や'*'も同様
-        if len(word) == 1 and unicodedata.name(word[0])[0:4] != 'CJK ':
-            continue
-        # 二文字のひらがなは無視
-        if (len(word) == 2 and unicodedata.name(word[0])[0:4] == 'HIRA'
-                and unicodedata.name(word[1])[0:4] == 'HIRA'):
-            continue
-        if (line[3][:2] == '名詞' or line[3][:2] == '動詞'
-                or line[3][:2] == '副詞' or line[3][:3] == '形容詞'):
-            ret_list.append(word)
-
-    return ret_list
-
-
-def make_pickle_from_json(fn='../meigens.json'):
-    '''
-    meigens.jsonを形態素解析し、辞書型の配列を作成・pickle保存
-    '''
-    print("making pickels...")
-    with open(fn, 'r', encoding='utf-8') as f:
-        meigenRowData = json.load(f)
-
-    meigenData = []
-    for meigen in meigenRowData:
-        data = {}
-        words = breakdown_into_validwords(meigen['body'])
-        title_words = breakdown_into_validwords(meigen['title'])
-        words += title_words
-        if len(words) <= 1:
-            continue
-
-        data['id'] = meigen['id']
-        data['url'] = meigen['image']
-        data['chr'] = meigen['character']
-        data['title'] = meigen['title']
-        data['meigen'] = meigen['body']
-        data['words'] = words
-        meigenData.append(data)
-        print('id = %d' % data['id'])
-
-    with open('meigenWords.bin', 'wb') as f:
-        pickle.dump(meigenData, f)
-
-    with open('meigenWords.csv', mode='w', encoding='utf-8') as f:
-        writer = csv.writer(f, lineterminator='\n')
-        for data in meigenData:
-            writer.writerow(data["words"])
 
 
 def main():
@@ -160,7 +36,7 @@ def main():
     else:
         tweet = sys.argv[1]
 
-    tweetWords = breakdown_into_validwords(tweet)
+    tweetWords = mecab_func.breakdown_into_validwords(tweet)
 
     min_r = 100.
     for meigen in meigenWords:
