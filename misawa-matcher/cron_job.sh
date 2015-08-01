e451e90f688d3dec03185a95d42d93d6b9de9603
garytaro <gary.taro@gmail.com>
2015-08-02 02:14:47 +0900

    誤ってコミットしたファイル郡を削除

--- misawa-matcher/cron_job.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-cd /home/ec2-user/horesase/misawa-matcher
-date > date.txt
-/usr/local/bin/python3 horesase_bot.py lsi300 model/lsi300.model model/jawiki_wordids.txt.bz2
-date >> date.txt
-# /usr/local/bin/python3 horesase_bot.py doc2vec model/d2v.model
